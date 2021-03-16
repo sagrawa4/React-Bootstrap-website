b6dd9491687c87481c1edc953003b3966e3b3c63
Shruti Agrawal <shruti98932@gmail.com>
2021-03-16 19:52:03 -0400

    Scroll Down imp

--- src/component/my-carousel/mycarousel.component.jsx
+++ src/component/my-carousel/mycarousel.component.jsx
@@ -3,7 +3,7 @@ import Carousel from "react-bootstrap/Carousel";
 import slide1 from '../../assets/img/carousal/slide1.webp';
 import slide2 from '../../assets/img/carousal/slide2.webp';
 import slide3 from '../../assets/img/carousal/slide3.webp';
-
+import ScrollDown from '../scroll-down/scroll-down.component';
 import "./mycarousel.styled.css";
 
 
@@ -24,6 +24,7 @@ const MyCarousel = () => {
       className="d-block w-100 custom-img" src={slide3} alt="Third slide" />
   </Carousel.Item>
 </Carousel>
+<ScrollDown/>
         </div>
     )
 }
