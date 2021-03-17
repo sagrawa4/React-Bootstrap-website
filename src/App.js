e5baebb685f6e524e888743fa4bbd82767e3dd2a
Shruti Agrawal <shruti98932@gmail.com>
2021-03-16 20:57:18 -0400

    About me links implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import './App.css';
+import Fade from 'react-reveal/Fade';
+import { Parallax } from "react-parallax";
 
 import MyNavbar from "./component/my-navbar/mynavbar.component.jsx";
 import MyCarousel from './component/my-carousel/mycarousel.component';
 import TitleMessage from './component/title-message/title-message.component';
 import About from "./pages/about/about.component";
+import Container from 'react-bootstrap/Container';
 
 const App =() => {
   return (
@@ -12,7 +15,22 @@ const App =() => {
       <MyNavbar />
       <MyCarousel/>
       <TitleMessage/>
-      <About/>
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+          > 
+        </Parallax>
+        <Container className="container-box rounded">
+        <Fade duration={500}>
+                <About />
+              </Fade>
+        </Container>
+        
+      </div>
+      
     </div>
   );
 }
