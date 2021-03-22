426f00568f01e98b57b704443c0149dd424e6c2b
Shruti Agrawal <shruti98932@gmail.com>
2021-03-22 12:15:20 -0400

    Styling for about updated

--- src/component/my-navbar/mynavbar.component.jsx
+++ src/component/my-navbar/mynavbar.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Logo from "../../assets/icons/my_opentowork_logo.png";
+import Logo from "../../assets/icons/Untitled document-1.png";
 import  './mynavbar.style.css';
 const MyNavbar = () => {
     return (
